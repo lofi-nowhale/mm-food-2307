f84d1b0f8669200fb024181907858feb4dc98269
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-10-27 10:26:55 -0600

    wip 2: it doesn't work still getting connection issues

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,14 @@
 class FoodsController < ApplicationController
   def index
-
     search_term = params[:q]
 
     conn = Faraday.new("https://api.nal.usda.gov") do |faraday|
       faraday.headers["X-Api-Key"] = Rails.application.credentials.foods[:key]
+      faraday.params['query'] = search_term
+      
     end
-
-    response = conn.get("/fdc/v1/foods/search?api_key=#{Rails.application.credentials.foods[:key]}&query=#{search_term}.json")
+    response = conn.get("/fdc/v1/foods/search?&query=#{search_term}")
+    require 'pry';binding.pry
 
     json = JSON.parse(response.body, symbolize_names: true)
     
--- spec/features/user_can_search_by_food_spec.rb
+++ spec/features/user_can_search_by_food_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "user search" do
   it 'allows user to search by ingredient' do
+    all_foods = File.read("spec/fixtures/foods_list.json")
+    
     stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/list?.json").
-      with(headers: {'X-API-Key' => Rails.application.credentials.foods[:key]}).
-      to_return(status: 200, body: '')
-
-     
+      with(headers: {'X-API-Key' => "#{Rails.application.credentials.foods[:key]}"}).
+      to_return(status: 200, body: all_foods)
 
     visit root_path
 
